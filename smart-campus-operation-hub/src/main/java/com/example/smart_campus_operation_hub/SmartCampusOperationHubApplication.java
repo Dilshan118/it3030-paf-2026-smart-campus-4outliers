4e332bbdc45fc364fc57b392409b04a4714fd00f
Dilshan Rajapakshe <dilshanrajapakshe8@gmail.com>
2026-03-21 14:29:43 +0530

    feat: initialize Smart Campus Operation Hub project setup

---
+++ smart-campus-operation-hub/src/main/java/com/example/smart_campus_operation_hub/SmartCampusOperationHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_campus_operation_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusOperationHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCampusOperationHubApplication.class, args);
+	}
+
+}
---
+++ smart-campus-operation-hub/src/test/java/com/example/smart_campus_operation_hub/SmartCampusOperationHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_campus_operation_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusOperationHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
